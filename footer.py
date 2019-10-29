b8e3e8407e173148e927ca5e5784b8397a8562b5
Benjamin Haettasch <benjamin.haettasch@fachschaft.informatik.tu-darmstadt.de>
2019-10-29 16:44:00 +0100

    Add settings for footer

---
+++ footer.py
@@ -0,0 +1,5 @@
+FOOTER_INFO = {
+    "repo_url": "https://gitlab.fachschaften.org/kif/akplanning",
+    "impress_text": "Organisator*innen der KIF",
+    "impress_url": "https://wiki.kif.rocks/wiki/KIF:Impressum"
+}
